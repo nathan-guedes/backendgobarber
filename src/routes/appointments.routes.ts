be165b56118cf19411ca22e0d8020653ecdc0b78
Doutir <nathan.guedes99@gmail.com>
2020-07-04 21:09:55 -0300

    backend Gobarber

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from 'typeorm';
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+// Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider_id, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      provider_id,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
